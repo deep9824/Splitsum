27eb40d8de377faf8c87918f39529d02adf55eb9
deep9824 <bhattdeep9898@gmail.com>
2024-12-03 11:12:13 +0530

    splash fix

--- ios/Splitsum/AppDelegate.mm
+++ ios/Splitsum/AppDelegate.mm
@@ -1,7 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-#import "RNSplashScreen.h"
+// #import "RNSplashScreen.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -11,8 +11,8 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-[RNSplashScreen show]; 
-  return YES;
+// [RNSplashScreen show]; 
+return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
